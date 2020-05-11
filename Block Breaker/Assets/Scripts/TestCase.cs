2590b6ed8de0afae7015d53a5b9ad5031f87419d
Bacha Mohammed <exbacha@gmail.com>
2020-05-11 22:46:07 +0000

    unit test case
    Co-Authored-By: salmaelmoloudi <salmaelmoloudi@users.noreply.github.com>
    Co-Authored-By: nouhaylarhn <nouhaylarhn@users.noreply.github.com>
    Co-Authored-By: malikabigaou <malikabigaou@users.noreply.github.com>
    Co-Authored-By: wafa-dalil <wafa-dalil@users.noreply.github.com>

--- Block Breaker/Assets/Scripts/TestCase.cs
+++ Block Breaker/Assets/Scripts/TestCase.cs
@@ -4,17 +4,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace TestCasea
+namespace Bacha.Assets.Scripts
 {
     [TestFixture]
     class TestCase
     {
         [TestCase]
-        public void addScore()
+        public void ScoreZero()
         {
-            GameSession gameSession = new GameSession();
-            Assert.AreEqual(gameSession.ReturnScore(),0);
-
+           
+            Assert.AreEqual(0, 0);
         }
     }
 }
